ce203b8593c72229ac4314570212747454d767a8
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-28 16:23:44 +0330

    add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
 
-const reminders:Reminder[]=[
-  {id:1,title:"reminder 1"}
-]
-
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: "reminder 1" },
+    { id: 2, title: "reminder 2" },
+    { id: 3, title: "reminder 3" },
+  ]);
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
