90e87a81c8686f108f0715aca608dc9a94e403a6
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-28 16:57:21 +0330

    get data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
+import getRemindersService from "./services/getRemindersService";
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    { id: 1, title: "reminder 1" },
-    { id: 2, title: "reminder 2" },
-    { id: 3, title: "reminder 3" },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    const getAllReminders = async () => {
+      try {
+        const { data } = await getRemindersService();
+        setReminders(data);
+      } catch (error) {}
+    };
+    getAllReminders();
+  }, []);
 
   return (
     <div className="App">
--- src/services/getRemindersService.ts
+++ src/services/getRemindersService.ts
@@ -2,7 +2,7 @@ import Reminder from "../models/reminder";
 import http from "./httpServices";
 
 const getRemindersService = () => {
-  return http.get<Reminder>("/todos");
+  return http.get<Reminder[]>("/todos");
 }
  
 export default getRemindersService;
